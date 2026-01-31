3eccd99d193b691183edc0065610c60b76d03a6d
duyenle1312 <lengomyduyen@gmail.com>
2026-02-01 01:13:22 +0200

    fix

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,17 +1,8 @@
 import DateFormatter from "@/components/DateFormatter";
 import markdownToHtml from "@/lib/markdownToHtml";
-import { getDocumentBySlug, getDocuments } from "outstatic/server";
+import { getDocumentBySlug } from "outstatic/server";
 
-// ✅ IMPORTANT: force static generation
 export const dynamic = "force-static";
-
-export async function generateStaticParams() {
-  const posts = getDocuments("posts");
-
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
 
 export default async function Post({
   params,
@@ -21,29 +12,39 @@ export default async function Post({
   const { slug } = params;
 
   const post = await getData(slug);
+  console.log("post", post);
 
   return (
-    <div className="max-w-6xl mx-auto px-5 py-12">
-      <article className="mb-32">
-        <h1 className="font-primary text-2xl font-bold md:text-4xl mb-2">
-          {post.title}
-        </h1>
+    <>
+      <div className="max-w-6xl mx-auto px-5 py-12">
+        <article className="mb-32">
+          {/* <div className="relative mb-2 md:mb-4 sm:mx-0 w-full h-52 md:h-96">
+            {<Image
+              alt={post.title}
+              src={post?.coverImage || ''}
+              fill
+              className="object-cover object-center"
+              priority
+            />} 
+          </div>*/}
 
-        <div className="hidden md:block md:mb-12 text-slate-600">
-          Written on <DateFormatter dateString={post?.publishedAt || ""} /> by{" "}
-          {post?.author?.name || ""}.
-        </div>
-
-        <hr className="border-neutral-200 mt-10 mb-10" />
-
-        <div className="max-w-2xl mx-auto">
-          <div
-            className="prose lg:prose-xl"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          />
-        </div>
-      </article>
-    </div>
+          <h1 className="font-primary text-2xl font-bold md:text-4xl mb-2">
+            {post.title}
+          </h1>
+          <div className="hidden md:block md:mb-12 text-slate-600">
+            Written on <DateFormatter dateString={post?.publishedAt || ""} /> by{" "}
+            {post?.author?.name || ""}.
+          </div>
+          <hr className="border-neutral-200 mt-10 mb-10" />
+          <div className="max-w-2xl mx-auto">
+            <div
+              className="prose lg:prose-xl"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            />
+          </div>
+        </article>
+      </div>
+    </>
   );
 }
 
@@ -54,7 +55,7 @@ async function getData(slug: string) {
     "slug",
     "author",
     "content",
-    "coverImage",
+    // "coverImage",
   ]);
 
   const content = await markdownToHtml(post?.content || "");
