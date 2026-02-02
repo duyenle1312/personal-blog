56bca2834304d275783b214758639a5f88f2fcbf
duyenle1312 <lengomyduyen@gmail.com>
2026-02-02 14:06:46 +0200

    fix

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,14 +1,13 @@
 import DateFormatter from "@/components/DateFormatter";
 import { Button } from "@/components/ui/button";
-import markdownToHtml from "@/lib/markdownToHtml";
 import { Metadata } from "next";
 import Link from "next/link";
 import { getDocuments } from "outstatic/server";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Duyen Le | Micro Blog",
-    description: "Welcome to my personal blog",
+    title: "Duyen Le | Gallery",
+    description: "Welcome to my personal gallery",
   };
 }
 
@@ -16,22 +15,23 @@ export default async function Posts() {
   const posts = await getData();
   // console.log("posts", posts);
 
-  const postsWithContent = await Promise.all(
-    posts.map(async (post) => ({
-      ...post,
-      content: await markdownToHtml(post?.content || ""),
-    })),
-  );
+  // const postsWithContent = await Promise.all(
+  //   posts.map(async (post) => ({
+  //     ...post,
+  //     content: await markdownToHtml(post?.content || ""),
+  //   })),
+  // );
 
   return (
     <div className="flex flex-col md:p-12 px-5 py-12">
       <h1 className="mb-8 text-4xl font-bold">All Posts</h1>
-      {postsWithContent.map((post) => (
+      {posts.map((post) => (
         <a key={post.slug} className="mb-5 block" href={`/posts/${post.slug}`}>
           <h3>{post.title}</h3>
           <div className="mb-2  text-slate-600">
             <DateFormatter dateString={post.publishedAt || ""} />{" "}
           </div>{" "}
+          <p>{String(post.image) || "No image available"}</p>
           <div
             dangerouslySetInnerHTML={{
               __html: post.content?.substring(0, 800) + "..." || "",
@@ -49,7 +49,7 @@ export default async function Posts() {
 }
 
 async function getData() {
-  const posts = getDocuments("imageposts", ["title", "content", "slug"]);
+  const posts = getDocuments("imageposts", ["title", "content", "slug", "publishedAt", "image"]);
 
   return posts;
 }
